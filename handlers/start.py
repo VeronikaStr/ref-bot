bcdef8c9d6f94cc8784b849669ab547b3d6a11f9
VeronikaStr <97550757+VeronikaStr@users.noreply.github.com>
2026-03-22 03:06:48 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,14 +1,16 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+from database import create_user
 from keyboards.menu import menu
 
 router = Router()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
+    user_id = message.from_user.id
+    await create_user(user_id)   # создаст пользователя, если его нет
     await message.answer(
-        "🎰 Добро пожаловать в Casino Pro!\n\n"
-        "Выберите игру:",
+        "🎰 Добро пожаловать в Casino Pro!",
         reply_markup=menu()
     )
