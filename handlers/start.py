b7fdabdbc77371da7667abbc64b9fb6f9b8cd4fc
VeronikaStr <97550757+VeronikaStr@users.noreply.github.com>
2026-03-22 03:16:14 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,7 +9,7 @@ router = Router()
 @router.message(Command("start"))
 async def cmd_start(message: Message):
     user_id = message.from_user.id
-    await create_user(user_id)   # создаст пользователя, если его нет
+    await create_user(user_id)
     await message.answer(
         "🎰 Добро пожаловать в Casino Pro!",
         reply_markup=menu()
